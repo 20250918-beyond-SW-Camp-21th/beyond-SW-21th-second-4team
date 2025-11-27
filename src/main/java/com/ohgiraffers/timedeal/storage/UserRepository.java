dcf48b8431e623b428eb481bf81ccaa0e46bddc0
cho-yunho01 <04whdbsgh30@naver.com>
2025-11-27 20:22:00 +0900

    feat(user): 로그인, 회원가입 API 추가

--- src/main/java/com/ohgiraffers/timedeal/storage/UserRepository.java
+++ src/main/java/com/ohgiraffers/timedeal/storage/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    Boolean existsUsernameAndPassword(String email, String password);
+
+    boolean existsByEmail(String email);
 }
