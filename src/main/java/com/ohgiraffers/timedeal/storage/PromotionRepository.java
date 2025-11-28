c598d497673b3b639bed2d1742de589b495d4232
yoocy0 <yoocy0328@gmail.com>
2025-11-28 20:17:02 +0900

    feat(order): Order api 구현

--- src/main/java/com/ohgiraffers/timedeal/storage/PromotionRepository.java
+++ src/main/java/com/ohgiraffers/timedeal/storage/PromotionRepository.java
@@ -2,7 +2,18 @@ package com.ohgiraffers.timedeal.storage;
 
 
 import com.ohgiraffers.timedeal.core.domain.Promotion;
+import com.ohgiraffers.timedeal.core.domain.product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface PromotionRepository extends JpaRepository<Promotion,Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Promotion p where p.id = :id")
+    public Optional<Promotion> findByIdWithPessimisticLock(Long id);
+
 }
