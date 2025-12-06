5ef69fb1f64b558850d06b3dad9da2d6530badd7
apeirogon99 <apeirogon99@gmail.com>
2025-12-06 23:18:47 +0900

    feat(project): account, queue, promotion, product, order 모듈 생성

---
+++ promotion/src/main/java/com/ohgiraffers/promotion/PromotionApplication.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
+public class PromotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromotionApplication.class, args);
+    }
+}
