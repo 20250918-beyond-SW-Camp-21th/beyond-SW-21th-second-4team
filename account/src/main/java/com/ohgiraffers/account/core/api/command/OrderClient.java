096883ff886fff86cf62382660b9d6831dfdc03f
cho-yunho01 <04whdbsgh30@naver.com>
2025-12-10 16:06:29 +0900

    perf(account): 클라이언트 수정

--- account/src/main/java/com/ohgiraffers/account/core/api/command/OrderClient.java
+++ account/src/main/java/com/ohgiraffers/account/core/api/command/OrderClient.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.account.core.api.command;
 
+import com.ohgiraffers.account.core.api.config.FeignClientConfig;
 import com.ohgiraffers.account.core.api.controller.v1.response.OrderDetailResponse;
 import com.ohgiraffers.common.support.response.ApiResult;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "timedeal-order-service")
+@FeignClient(name = "timedeal-order-service" , configuration = FeignClientConfig.class)
 public interface OrderClient {
 
     @GetMapping("/api/v1/orders/me/orders")
